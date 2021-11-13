ddfb59a193633d89fb1958321ef9ce357dd2422e
Thomas Churchman <thomas@kepow.org>
2021-11-13 10:25:20 +0100

    Flake: fix devshell

--- flake.nix
+++ flake.nix
@@ -7,32 +7,36 @@
 
   outputs = { flake-utils, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inputs =
+          with pkgs; let
+            python-packages = python-packages: [
+              python-packages.requests
+              python-packages.imageio
+              python-packages.imageio-ffmpeg
+            ];
+            python-with-packages = python3.withPackages python-packages;
+          in
+          [
+            python-with-packages
+            ffmpeg
+            jpeg-archive
+          ];
       in
       rec {
         packages = {
           phone-camera-upload = pkgs.python3Packages.buildPythonApplication
             rec {
-              propagatedBuildInputs =
-                with pkgs; let
-                  python-packages = python-packages: [
-                    python-packages.requests
-                    python-packages.imageio
-                    python-packages.imageio-ffmpeg
-                  ];
-                  python-with-packages = python3.withPackages python-packages;
-                in
-                [
-                  python-with-packages
-                  ffmpeg
-                  jpeg-archive
-                ];
+              propagatedBuildInputs = inputs;
               pname = "phone-camera-upload";
               version = "0.1.0";
               src = ./.;
             };
         };
         defaultPackage = packages.phone-camera-upload;
-        devShell = packages.phone-camera-upload;
+        devShell = pkgs.mkShell {
+          buildInputs = inputs;
+        };
       });
 }
